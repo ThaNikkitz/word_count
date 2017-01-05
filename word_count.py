baf25d6c7fce0f904128d49da4fac02b6730591c
Nico Espinoza <nico.espinozam@gmail.com>
2017-01-05 11:47:50 -0300

    Agregada una función para hacer correr el contador de palabras.

--- word_count.py
+++ word_count.py
@@ -1,9 +1,15 @@
-happy = input("Escribe algo para contar el número de palabras: ")
+def word_count(happy):
 
-words = happy.split()
+	happy = input("Escribe algo para contar el número de palabras: ")
 
-counts = {}
-for word in words:
-    counts[word] = counts.get(word, 0) + 1
-print("La cantidad de palabras de lo que escribió el usuario es: ")
-print(counts)
+	words = happy.split()
+
+	counts = {}
+	for word in words:
+	    counts[word] = counts.get(word, 0) + 1
+	print("La cantidad de palabras de lo que escribió el usuario es: ")
+	print(counts)
+def main():
+	return word_count(happy)
+
+main()
